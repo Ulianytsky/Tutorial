4f9a1e5b18289de363858f1aa5ee532b9dd6e9ee
Ulianytsky <123118484+Ulianytsky@users.noreply.github.com>
2023-01-22 13:25:20 +0100

    dz

---
+++ complex.py
@@ -0,0 +1,4 @@
+a = -2 + 3j
+b = 4 + 2.1j
+result = a + b
+print (result)
---
+++ f-strings.py
@@ -0,0 +1,5 @@
+# name_1 = 90
+# name_2 = 87
+# example = f"Symple exampl {name_1} + {name_2} = {name_1 + name_2}"
+# print(example)
+
